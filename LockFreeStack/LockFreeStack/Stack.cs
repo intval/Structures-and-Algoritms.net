b3ab70ab75cd49c7fec9c1554b48e410e67d73d2
Alex (win) <alex@phpguide.co.il>
2013-03-28 13:56:24 +0200

    Lock Free Stack implementation with interlocked

---
+++ LockFreeStack/LockFreeStack/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LockFreeStack
+{
+    public class Stack
+    {
+        private Node head;
+
+        public void Push(int n)
+        {
+            Node newNode = new Node(n);
+            Node curHead;
+
+            do 
+            {
+                curHead = head;
+            } 
+            while(curHead != Interlocked.CompareExchange(ref head, newNode, curHead));
+
+            newNode.next = curHead;
+        }
+
+        public int Pop()
+        {
+            Node retNode;
+
+            do
+            {
+                retNode = head;
+            }
+            while(head != Interlocked.CompareExchange(ref head, head.next, retNode));
+
+            if (null == retNode)
+                throw new NullReferenceException();
+            else
+                return retNode.value;
+        }
+    }
+
+    class Node
+    {
+        public int value;
+        public Node next;
+
+        public Node(int val)
+        {
+            this.value = val;
+        }
+    }
+}
---
+++ LockFreeStack/StackTest/UnitTest1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LockFreeStack;
+using System.Threading.Tasks;
+
+namespace StackTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        /// </summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void CheckItWorks()
+        {
+            Stack s = new Stack();
+            s.Push(1);
+            s.Push(2);
+
+            Assert.AreEqual(2, s.Pop());
+            Assert.AreEqual(1, s.Pop());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void EmptyStackThrows()
+        {
+            Stack s = new Stack();
+            s.Pop();
+        }
+
+        [TestMethod]
+        public void ParallelInsertion()
+        {
+            int min = 1, max = 200;
+
+            var s = new Stack();
+
+            var options = new ParallelOptions() 
+            { 
+                MaxDegreeOfParallelism = 30
+            };
+
+            Parallel.For(min, max, options, i => { s.Push(i); });
+
+            for (int i = min; i < max; i++)
+            {
+                int item = s.Pop();
+                Assert.IsTrue(item >= min && item < max);
+            }
+
+            bool caught = false;
+
+            try
+            {
+                s.Pop();
+            }
+            catch (NullReferenceException)
+            {
+                caught = true;
+            }
+
+            Assert.IsTrue(caught, "Should not have more items in stack then parallel for has insterted");
+        }
+
+        [TestMethod]
+        public void interMixedInsertionDeletion()
+        {
+            int min = 1, max = 300;
+
+            var s = new Stack();
+
+            var options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = 30
+            };
+
+            Parallel.For(min, max, options, i => {
+
+                s.Push(i);
+
+                if (i % 3 == 0)
+                    s.Pop();
+            
+            });
+        }
+
+    }
+}
